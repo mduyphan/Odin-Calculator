9734d6ba4b5ed14816028f05373a76f5c7657f0a
mduyphan <89181175+mduyphan@users.noreply.github.com>
2025-03-17 18:15:38 +0700

    Add basic math functions

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-foo
+const restArray = (array) => array.slice(1, array.length);
+
+let number1;
+let number2;
+let operation;
+
+const add = function(firstNumber, secondNumber) {
+   
+    return firstNumber + secondNumber;
+};
+
+const subtract = function(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+
+const multiply = function(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+
+const divide = function(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+};
+
+const operate = (operation, number1, number2) => {
+    switch (operation) {
+    case "+":
+	return add(number1, number2);
+    case "-":
+	return subtract(number1, number2);
+    case "*":
+	return multiply(number1, number2);
+    case "/":
+	return divide(number1, number2);
+    }
+};
+
+
