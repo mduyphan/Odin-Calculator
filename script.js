b8d9b9a12a5c311c0f344a9c124d4ad67f3ebcf2
mduyphan <89181175+mduyphan@users.noreply.github.com>
2025-03-19 16:43:50 +0700

    Add ability for number display to reset when an operation is clicked and another digit is entered

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 // Constant
 const restArray = (array) => array.slice(1, array.length);
 
+let currentOperation;
 let number1;
 let number2;
-let operation;
 
+
+const operations = document.querySelectorAll(".button.operation");
 const numberButtons = document.querySelectorAll(".number-boxes .button");
 const numberDisplay = document.querySelector("#number-display");
 const negativeButton = document.querySelector(".button.negative");
@@ -58,7 +60,13 @@ const operate = (operation, number1, number2) => {
 // Display the clicked numbers on screen
 for (let i of numberButtons) {
     i.addEventListener("click", () => {
-	numberDisplay.textContent += `${i.textContent}`;
+	if ((numberDisplay.textContent.length <= 13) &&
+	    ((number1 == null) || !(number2 == null))) {
+	    numberDisplay.textContent += `${i.textContent}`;
+	} else {
+	    numberDisplay.textContent = `${i.textContent}`;
+	    number2 = numberDisplay.textContent;
+	}
     });
 };
 
@@ -69,3 +77,19 @@ negativeButton.addEventListener("click", () => {
 	numberDisplay.textContent = `-${numberDisplay.textContent}`;
     } else {numberDisplay.textContent = numberDisplay.textContent.slice(1)};
 });
+
+// clickEvent ->
+// Perform calculation base on clicked operation button
+for (let i of operations) {
+    i.addEventListener("click", () => {
+	if (currentOperation == null) {
+	    number1 = Number(numberDisplay.textContent);
+	    currentOperation = i.textContent;
+	} else if (number2 !== null) {
+	    numberDisplay.textContent = `${operate(currentOperation, number1, number2)}`
+	} else {
+	    number2 = number1;
+	    number1 = null;
+	}
+    })
+};
