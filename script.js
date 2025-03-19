f3f94637fc3b90017697966e708bd3288e18fdb0
mduyphan <89181175+mduyphan@users.noreply.github.com>
2025-03-19 14:51:40 +0700

    Add number display with negative toggle button

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
+// ================
+// Constant
 const restArray = (array) => array.slice(1, array.length);
 
 let number1;
 let number2;
 let operation;
 
+const numberButtons = document.querySelectorAll(".number-boxes .button");
+const numberDisplay = document.querySelector("#number-display");
+const negativeButton = document.querySelector(".button.negative");
+// ================
+// Data Definition
+// Operation is one of:
+//	- "+"
+//	- "-"
+//	- "*"
+//	- "/"
+
+// ================
+// Function
+
+// Number Number -> Number
+// Basic Math Operations Functions
 const add = function(firstNumber, secondNumber) {
    
     return firstNumber + secondNumber;
@@ -21,6 +39,8 @@ const divide = function(firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 };
 
+// Operation Number Number -> Number
+// Return the result between number1 and number2 by math operation
 const operate = (operation, number1, number2) => {
     switch (operation) {
     case "+":
@@ -34,4 +54,18 @@ const operate = (operation, number1, number2) => {
     }
 };
 
+// clickEvent -> "number-display"Element
+// Display the clicked numbers on screen
+for (let i of numberButtons) {
+    i.addEventListener("click", () => {
+	numberDisplay.textContent += `${i.textContent}`;
+    });
+};
 
+// clickEvent -> "number-display"Element
+// Append Negative sign at the start of curren number value or Remove it from current number on click
+negativeButton.addEventListener("click", () => {
+    if (!isNaN(Number(numberDisplay.textContent.at(0)))) {
+	numberDisplay.textContent = `-${numberDisplay.textContent}`;
+    } else {numberDisplay.textContent = numberDisplay.textContent.slice(1)};
+});
