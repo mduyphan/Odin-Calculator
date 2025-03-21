ccbdfaa0f4388c709f63aa16239f1049429db65f
mduyphan <89181175+mduyphan@users.noreply.github.com>
2025-03-21 21:18:41 +0700

    Add all basic functions not including extra credits Remaining bug: accumulative calculation after clicking equal. Consider reduce method to fix

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 // Constant
 const restArray = (array) => array.slice(1, array.length);
 
-let currentOperation;
-let number1;
-let number2;
+let currentOperation = [];
+let numberArray = [];
+let number2 = [];
 
 
 const operations = document.querySelectorAll(".button.operation");
@@ -32,7 +32,7 @@ const add = function(firstNumber, secondNumber) {
 };
 
 const subtract = function(firstNumber, secondNumber) {
-    return Number(firstNumber) - Number(secondNumber);
+    return Number(secondNumber) - Number(firstNumber);
 };
 
 const multiply = function(firstNumber, secondNumber) {
@@ -40,83 +40,89 @@ const multiply = function(firstNumber, secondNumber) {
 };
 
 const divide = function(firstNumber, secondNumber) {
-    return Number(firstNumber) / Number(secondNumber);
+    return  Math.round((Number(secondNumber) / Number(firstNumber)) * 1000000000000) /1000000000000;
 };
 
 // Operation Number Number -> Number
-// Return the result between number1 and number2 by math operation
-const operate = (operation, number1, number2) => {
+// Return the result between numberArray and number2 by math operation
+const operate = (operation, numberArray, number2) => {
     switch (operation) {
     case "+":
-	return add(number1, number2);
+	return add(numberArray, number2);
     case "-":
-	return subtract(number1, number2);
+	return subtract(numberArray, number2);
     case "x":
-	return multiply(number1, number2);
+	return multiply(numberArray, number2);
     case "/":
-	return divide(number1, number2);
+	return divide(numberArray, number2);
     }
 };
 
 // clickEvent -> "number-display"Element
 // Display the clicked numbers on screen
+let value1;
+let value2;
 for (let i of numberButtons) {
     i.addEventListener("click", () => {
-	if ((numberDisplay.textContent.length <= 13) &&
-	    ((number1 == null) || !(number2 == null))) {
-	    numberDisplay.textContent += `${i.textContent}`;
-	    number2 = numberDisplay.textContent;
-	} else {
-	    numberDisplay.textContent = `${i.textContent}`;
-	    number2 = numberDisplay.textContent;
-	}
+	if (numberArray.length <= 12) {
+	    numberArray.push(i.textContent);
+	};
+	value1 = numberArray.join("");
+	numberDisplay.textContent =  value1;
     });
 };
+		      
 
 // clickEvent -> "number-display"Element
 // Append Negative sign at the start of curren number value or Remove it from current number on click
 negativeButton.addEventListener("click", () => {
-    if (!isNaN(Number(numberDisplay.textContent.at(0)))) {
-	numberDisplay.textContent = `-${numberDisplay.textContent}`;
-	number1 = numberDisplay.textContent;
+    if (!(numberArray[0] == "-")) {
+	numberArray.unshift("-");
     } else {
-	numberDisplay.textContent = numberDisplay.textContent.slice(1)
-	number1 = numberDisplay.textContent;};
+	numberArray.shift();
+    };
+    value1 = numberArray.join("");
+    numberDisplay.textContent = value1;
 });
 
-// clickEvent ->
+// clickEvent -> Operation
 // Perform calculation base on clicked operation button
 for (let i of operations) {
     i.addEventListener("click", () => {
-	if (currentOperation == null) {
-	    number1 = numberDisplay.textContent;
-	    currentOperation = i.textContent;
-	    number2 = null;
-	// } else if (!(currentOperation == null)) {
-	//     currentOperation = i.textContent;
-	} else if (!(number1 == null)) {
-	    number2 = number1;
-	    number1 = numberDisplay.textContent;
-	    numberDisplay.textContent = `${operate(currentOperation, number1, number2)}`;
-	    currentOperation = i.textContent;
-	    number1 = numberDisplay.textContent;
-	    number2 = null;
-	};
-    })
+	if (currentOperation.length == 0) {
+	    currentOperation.push(i.textContent);
+	    console.log(currentOperation[0]);
+	} else {
+	    currentOperation[0] = i.textContent;
+	    console.log(currentOperation[0]);
+	}
+	if (value2 == null) {
+	    value2 = value1;
+	    numberArray = [];
+	} else {
+	    value2 = operate(currentOperation[0], value1, value2);
+	    numberDisplay.textContent = value2;
+	    numberArray = [];
+	    currentOperation = [];
+	};;
+    });
+
 };
 
 equalButton.addEventListener("click", () => {
-    if (!(currentOperation == null)) {
-	numberDisplay.textContent = `${operate(currentOperation, number1, number2)}`;
-	currentOperation = null;
-	number1 = numberDisplay.textContent;
-	number2 = null;
-    }
+    if (!(value2 == null)) {
+	value2 = operate(currentOperation[0], value1, value2);
+	numberDisplay.textContent = value2;
+	value1 = value2;
+	numberArray = [];
+	currentOperation = [];
+	value2 = null;
+    } else numberDisplay.textContent; 
 });
 
 resetButton.addEventListener("click", () => {
     numberDisplay.textContent = null;
-    currentOperation = null;
-    number1 = null;
-    number2 = null;
+    currentOperation = [];
+    numberArray = [];
+    value2 = null;
 });
